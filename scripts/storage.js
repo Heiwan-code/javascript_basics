3b317196661c5f16e85acc9de442f710b124b729
Heiwan-code <heiwan.code@gmail.com>
2023-02-20 19:19:13 +0200

    js objects

--- scripts/storage.js
+++ scripts/storage.js
@@ -1,4 +1,5 @@
 const storage = {
+
     vegetables: {
         carrots: 0,
         potatoes: 0,
@@ -6,6 +7,7 @@ const storage = {
         gurkis: 0,
         kirbis: 0
     },
+
     addSingle(btn, vegetable) {
 
         const vegetableInputField = btn.parentElement.querySelector('input')
@@ -15,21 +17,23 @@ const storage = {
         this.vegetables[vegetable] += ~~vegetableAddValue
         console.log(this.vegetables[vegetable])
     },
+
     addSinglePredefined(vegetable, howMuch) {
         this.vegetables[vegetable] += ~~howMuch
         console.log(this.vegetables[vegetable])
     },
 
     seeAll() {
-
+        
         // console.log(this.storage)
-
         for (const [key, value] of Object.entries(this.vegetables)) {
             console.log(`${key}: ${value}`);
         }
-
         // console.log("carrots", this.carrots)
         // console.log("potatoes", this.potatoes)
         // console.log("tomatoes", this.tomatoes)
+    
     }
+
+    
 }
